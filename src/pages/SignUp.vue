77ef2215fc9da85ef44c3c08687a80ceee6e5348
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-05-29 15:15:11 +0700

    feat(signup): add createUser() to create new user account

--- src/pages/SignUp.vue
+++ src/pages/SignUp.vue
@@ -15,7 +15,35 @@ const newUser: Ref<User> = ref({
     confirmPassword: "",
 })
 
-function createUser() {
+async function createUser() {
+    if (newUser.value.password !== newUser.value.confirmPassword) {
+        alert("password does not match")
+        return
+    }
+
+    let requestBody = {
+        email: newUser.value.email,
+        username: newUser.value.username,
+        password: newUser.value.password
+    }
+
+    let url = "http://localhost:8000/users"
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json",
+        },
+        body: JSON.stringify(requestBody)
+    })
+
+    if (!response.ok) {
+        alert("create user failed")
+        return
+    }
+
+    alert("create user successful")
+
     newUser.value = {
         email: "",
         username: "",
@@ -31,19 +59,19 @@ function createUser() {
     <form @submit.prevent="createUser">
         <div>
             <label for="email">Email</label>
-            <input type="email" name="email" v-model="newUser.email">
+            <input type="email" required name="email" v-model="newUser.email">
         </div>
         <div>
             <label for="username">Username</label>
-            <input type="text" name="username" v-model="newUser.username">
+            <input type="text" required name="username" v-model="newUser.username">
         </div>
         <div>
             <label for="password">Password</label>
-            <input type="text" name="password" v-model="newUser.password">
+            <input type="password" required name="password" v-model="newUser.password">
         </div>
         <div>
             <label for="confirm_password">Confirm Password</label>
-            <input type="text" name="confirm_password" v-model="newUser.password">
+            <input type="password" required name="confirm_password" v-model="newUser.confirmPassword">
         </div>
     <button>Sign Up</button>
     </form>
