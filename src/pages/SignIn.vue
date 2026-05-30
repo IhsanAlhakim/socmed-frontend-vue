d4ba4b3693f2b43b6467b399db0b094a5a6d547a
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-05-30 16:10:06 +0700

    feat(sign in): create sign in page, sign in form, and signIn() for sign in to the website

---
+++ src/pages/SignIn.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+
+interface SignInUser {
+    email: string,
+    password: string
+}
+
+const signInUser: Ref<SignInUser> = ref({
+    email: "",
+    password: "",
+})
+
+let router = useRouter()
+
+async function signIn(){
+    let requestBody: SignInUser = {
+        email: signInUser.value.email,
+        password: signInUser.value.password
+    }
+
+    console.log(requestBody)
+
+    let url = "http://localhost:8000/sessions"
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        credentials: "include",
+        body: JSON.stringify(requestBody)
+    })
+
+    if (!response.ok) {
+        alert("sign in failed")
+        return
+    }
+    router.push("/home")
+}
+
+</script>
+
+<template>
+<h2>Sign In</h2>
+<form @submit.prevent="signIn">
+    <div>
+        <label for="email">Email</label>
+        <input type="email" required name="email" v-model="signInUser.email">
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input type="password" required name="password" v-model="signInUser.password">
+    </div>
+    <button>Sign In</button>
+</form>
+<RouterLink to="/signup">Create New Account</RouterLink>
+</template>
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,10 +1,10 @@
 import Home from "@/pages/Home.vue"
-import Login from "@/pages/Login.vue"
+import SignIn from "@/pages/SignIn.vue"
 import SignUp from "@/pages/SignUp.vue"
 import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router"
 
 const routes: RouteRecordRaw[] = [
-    {path: "/", component: Login},
+    {path: "/", component: SignIn},
     {path: "/signup", component: SignUp},
     {path: "/home", component: Home},
 ]
