4d255a50a1a487d961f02662f19bd991a14b11d3
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-06-02 17:22:37 +0700

    feat(Home): add navigation guard to keep home page from unauthorized user

--- src/router/router.ts
+++ src/router/router.ts
@@ -3,10 +3,35 @@ import SignIn from "@/pages/SignIn.vue"
 import SignUp from "@/pages/SignUp.vue"
 import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router"
 
+let isAuthenticated = false
+
 const routes: RouteRecordRaw[] = [
     {path: "/", component: SignIn},
     {path: "/signup", component: SignUp},
-    {path: "/home", component: Home},
+    {
+        path: "/home", 
+        component: Home,
+        beforeEnter: async (to, from) => {
+            try {
+                let url = "http://localhost:8000/users"
+                const response = await fetch(url, {
+                method: "GET",
+                headers: {
+                "Content-Type":"application/json"
+                },
+                credentials: "include",
+                })
+
+                if (!response.ok) {
+                    throw new Error("something went wrong")
+                }
+            } catch (error) {
+                console.log("something went wrong")
+                return {path: "/"}
+            }
+
+        }
+    },
 ]
 
 const router = createRouter({
