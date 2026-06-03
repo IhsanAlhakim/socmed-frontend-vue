c83e3d863b334ff076505bda2b27fdbd34988f1d
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-06-03 18:06:17 +0700

    feat(Home): use userData from localStorage in home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,1 +1,26 @@
-<template>Home</template>
+<script setup lang="ts">
+import type { UserData } from '@/types/user';
+import { ref, type Ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+let userDataObjStr = localStorage.getItem("userObj")
+
+let userDataObj
+
+if (!userDataObjStr) {
+    router.push("/")
+} else {
+    userDataObj = JSON.parse(userDataObjStr)
+}
+
+const userData: Ref<UserData> = ref(userDataObj)
+
+</script>
+<template>
+    <aside>
+        <p>Hello {{ userData.username }}</p>
+    </aside>
+    <main>Home</main>
+</template>
---
+++ src/types/user.ts
@@ -0,0 +1,6 @@
+export interface UserData {
+    id: number,
+    username: string,
+    email: string,
+    created_at: string
+}
