4800483a231d09152e040551992091033e83544c
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-06-10 16:46:59 +0700

    refactor: move root route guard logic into separate file

--- src/router/router.ts
+++ src/router/router.ts
@@ -4,6 +4,7 @@ import SignIn from "@/pages/SignIn.vue"
 import SignUp from "@/pages/SignUp.vue"
 import type { APIResponse } from "@/types/responseJson"
 import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router"
+import { beforeEnterRoot } from "./before_enter_func"
 
 const routes: RouteRecordRaw[] = [
     {path: "/signin", component: SignIn},
@@ -14,33 +15,7 @@ const routes: RouteRecordRaw[] = [
         children: [
             {path: "home", component: Home}
         ],
-        beforeEnter: async (to, from) => {
-            try {
-                let url = "http://localhost:8000/users"
-                
-                const response = await fetch(url, {
-                method: "GET",
-                headers: {
-                "Content-Type":"application/json"
-                },
-                credentials: "include",
-                })
-
-                if (!response.ok) {
-                    throw new Error("something went wrong")
-                }
-
-                let responseJson: APIResponse = await response.json()
-
-                localStorage.setItem("userObj", JSON.stringify(responseJson.data))
-
-            } catch (error) {
-                console.log("something went wrong")
-                localStorage.removeItem("userObj")
-                return {path: "/signin"}
-            }
-
-        }
+        beforeEnter: beforeEnterRoot,
     },
 ]
 
