94b6238ee42b71645981eb8537cd28914f2cd39c
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-07-11 14:46:53 +0700

    feat(PostItem): add "delete post" feature

--- src/components/home/PostItem.vue
+++ src/components/home/PostItem.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
+import { loggedInUserKey } from '@/config/injectionKeys';
 import type { Post } from '@/types/post';
-import { ref } from 'vue';
+import { inject, ref } from 'vue';
 import { useRouter } from 'vue-router';
 interface Props {
     post: Post
@@ -12,12 +13,15 @@ const postLikeCount = ref(props.post.like_count)
 
 const router = useRouter()
 
+const loggedInUser = inject(loggedInUserKey)
+
+
 async function likeHandler() {
     if (postLikes.value == false) {
         try {
             postLikes.value = true
             postLikeCount.value = postLikeCount.value + 1
-            
+
 
             const url = `http://localhost:8000/posts/${props.post.id}/likes`
 
@@ -40,7 +44,7 @@ async function likeHandler() {
         try {
             postLikes.value = false
             postLikeCount.value = postLikeCount.value - 1
-            
+
             const url = `http://localhost:8000/posts/${props.post.id}/likes`
 
             const response = await fetch(url, {
@@ -61,16 +65,49 @@ async function likeHandler() {
 
 }
 
+async function deletePostHandler() {
+    if (!confirm("delete this post?")) {
+        return
+    }
+
+    if (props.post.user_id !== loggedInUser?.id) {
+        alert("cannot delete this post")
+        return
+    }
+
+    try {
+        const url = `http://localhost:8000/posts/${props.post.id}`
+
+        const response = await fetch(url, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+        })
+
+        if (!response.ok) {
+            throw new Error("something went wrong")
+        }
+        alert("post deleted")
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 </script>
 <template>
-        <div class="mb-4 cursor-pointer" @click="router.push({ path: `/p/${post.id}` })">
-            <p @click.stop="router.push({path: `/u/${post.creator}`})">{{ post.creator }}</p> 
-            <p>{{ post.content }}</p>
-            <div class="flex">
-                <button v-if="postLikes" class="border-2 cursor-pointer" @click.stop="likeHandler">liked</button>
-                <button v-else class="border-2 cursor-pointer" @click.stop="likeHandler">like</button>
-                <p>: {{ postLikeCount }}</p>
-            </div>
-            <p>comment : {{ post.comment_count }}</p>
+    <div class="mb-4 cursor-pointer" @click="router.push({ path: `/p/${post.id}` })">
+        <button v-if="loggedInUser && loggedInUser.id === post.user_id" class="border-2 cursor-pointer bg-red-500"
+            @click.stop="deletePostHandler">Delete Post</button>
+        <p @click.stop="router.push({ path: `/u/${post.creator}` })">{{ post.creator }}</p>
+        <p>{{ post.content }}</p>
+        <div class="flex">
+            <button v-if="postLikes" class="border-2 cursor-pointer" @click.stop="likeHandler">liked</button>
+            <button v-else class="border-2 cursor-pointer" @click.stop="likeHandler">like</button>
+            <p>: {{ postLikeCount }}</p>
         </div>
+        <p>comment : {{ post.comment_count }}</p>
+    </div>
 </template>
