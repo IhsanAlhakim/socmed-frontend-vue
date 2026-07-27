356e3e4b8e38572482e51b34a9cbe0ad62e73e84
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-07-27 17:51:45 +0700

    refactor: create FormInputBox component for sign up and sign in form

---
+++ src/components/FormInputBox.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Props {
+    type: string
+    name: string
+    title: string
+}
+
+const props = defineProps<Props>()
+const model = defineModel()
+
+</script>
+
+<template>
+<div class="flex flex-col gap-1">
+    <label :for="name">{{ title }}</label>
+    <input :type="type" required :name="name" v-model="model" class="w-130 h-10 px-3 py-1 border-2 rounded-md">
+</div>
+</template>
---
+++ src/components/FormInputBoxUnderHood.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+    title: string
+    type: string
+    name: string
+    modelValue: string
+}
+
+const props = defineProps<Props>()
+const emit = defineEmits(['update:modelValue'])
+
+function onInputHandler(event: Event) {
+    const target = event.target as HTMLInputElement
+    emit('update:modelValue', target.value)
+}
+
+</script>
+
+<template>
+<div class="flex flex-col gap-1">
+    <label :for="name">{{ title }}</label>
+    <input :type="type" required :name="name" :value="modelValue" @input="onInputHandler"  class="w-130 h-10 px-3 py-1 border-2 rounded-md">
+</div>
+</template>
