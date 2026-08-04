c04778930d278395c673aa41945924056bd79340
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-08-04 13:39:22 +0700

    feat(sign out): show confirm dialog after click sign out button

--- src/layouts/RootLayout.vue
+++ src/layouts/RootLayout.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 import { signOut } from '@/api/users'
 import Button from '@/components/Button.vue'
+import ShadButton from '@/components/ui/button/ShadButton.vue'
+import Dialog from '@/components/ui/dialog/Dialog.vue'
+import DialogClose from '@/components/ui/dialog/DialogClose.vue'
+import DialogContent from '@/components/ui/dialog/DialogContent.vue'
+import DialogDescription from '@/components/ui/dialog/DialogDescription.vue'
+import DialogFooter from '@/components/ui/dialog/DialogFooter.vue'
+import DialogHeader from '@/components/ui/dialog/DialogHeader.vue'
+import DialogTitle from '@/components/ui/dialog/DialogTitle.vue'
+import DialogTrigger from '@/components/ui/dialog/DialogTrigger.vue'
 import { loggedInUserKey } from '@/config/injectionKeys'
-import { unknownErrorMessage } from '@/errors/unknown-error'
 import type { UserData } from '@/types/user'
 import { Home } from '@lucide/vue'
 import { provide, ref, type Ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
+const isLoading = ref(false)
 
 let userDataObjStr = localStorage.getItem("userObj")
 
@@ -25,11 +34,8 @@ const userData: Ref<UserData> = ref(userDataObj)
 provide(loggedInUserKey, userDataObj)
 
 async function signOutButtonHandler() {
-    if (!confirm("are you sure you want to sign out?")) {
-        return
-    }
-
     try {
+        isLoading.value = true
         const signOutResponse = await signOut()
 
         if (!signOutResponse.ok) {
@@ -40,34 +46,59 @@ async function signOutButtonHandler() {
     } catch (error) {
         console.log(error)
         alert("signOut failed")
+    } finally {
+        isLoading.value = false
     }
 }
 </script>
 
 <template>
-    <div class="relative w-full grid grid-cols-[25%_50%_25%] bg-[#030712] text-white">
-        <aside class="fixed w-[25%] h-dvh border-r">
-            <div class="h-full flex flex-col py-5 pl-15 pr-10">
-                <h1 class="text-4xl font-bold">Socmed</h1>
-                <ul class="flex mt-5 text-2xl font-semibold items-center gap-5">
-                    <Home :size="30"/>
-                    <li>Home</li>
-                </ul>
-                <div class="mt-auto flex flex-col gap-5">
-                    <p class="text-center text-lg">Hello, {{ userData.username }}</p>
-                    <Button @click="signOutButtonHandler" class="bg-red-700 hover:bg-red-500 text-lg">Sign Out</Button>
+    <Dialog>
+        <div class="relative w-full grid grid-cols-[25%_50%_25%] bg-[#030712] text-white">
+            <aside class="fixed w-[25%] h-dvh border-r">
+                <div class="h-full flex flex-col py-5 pl-15 pr-10">
+                    <h1 class="text-4xl font-bold">Socmed</h1>
+                    <ul class="flex mt-5 text-2xl font-semibold items-center gap-5">
+                        <Home :size="30" />
+                        <li>Home</li>
+                    </ul>
+                    <div class="mt-auto flex flex-col gap-5 items-center">
+                        <p class="text-center text-lg">Hello, {{ userData.username }}</p>
+                        <DialogTrigger as-child>
+                            <Button class="w-40 bg-red-700 hover:bg-red-500 text-lg">Sign
+                                Out</Button>
+                        </DialogTrigger>
+                    </div>
                 </div>
-            </div>
-        </aside>
-        <!-- Div untuk mengisi posisi aside yang fixed -->
-        <div></div> 
-        <!-- Div untuk mengisi posisi aside yang fixed -->
-        <main class="min-h-dvh">
-            <RouterView />
-        </main>
-         <!-- Div untuk mengisi posisi aside yang fixed -->
-        <div></div> 
-        <!-- Div untuk mengisi posisi aside yang fixed -->
-        <aside class="right-0 fixed w-[25%] h-dvh border-l"></aside>
-    </div>
+            </aside>
+            <!-- Div untuk mengisi posisi aside yang fixed -->
+            <div></div>
+            <!-- Div untuk mengisi posisi aside yang fixed -->
+            <main class="min-h-dvh">
+                <RouterView />
+            </main>
+            <!-- Div untuk mengisi posisi aside yang fixed -->
+            <div></div>
+            <!-- Div untuk mengisi posisi aside yang fixed -->
+            <aside class="right-0 fixed w-[25%] h-dvh border-l"></aside>
+        </div>
+        <DialogContent class="bg-[#030712] text-white">
+            <DialogHeader>
+                <DialogTitle>Sign out</DialogTitle>
+                <DialogDescription>
+                    Are you sure you want to sign out?
+                </DialogDescription>
+            </DialogHeader>
+            <DialogFooter class="text-sm">
+                <DialogClose as-child>
+                <Button class="bg-red-700 hover:bg-red-500">
+                    Cancel
+                </Button>
+                </DialogClose>
+                <Button @click="signOutButtonHandler" :is-disabled="isLoading" :class="[isLoading ? 'bg-gray-700' : 'bg-sky-700 hover:bg-sky-500']">
+                Sign Out
+                </Button>
+            </DialogFooter>
+        </DialogContent>
+    </Dialog>
 </template>
